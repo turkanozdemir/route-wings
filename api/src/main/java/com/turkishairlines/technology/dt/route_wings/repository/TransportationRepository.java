c3232bc33440fcfca6862b26a9397e0dfff24123
Türkan Özdemir <83505749+turkanozdemir@users.noreply.github.com>
2025-03-31 15:24:21 +0300

    feat: add Location and Transportation repositories

---
+++ api/src/main/java/com/turkishairlines/technology/dt/route_wings/repository/TransportationRepository.java
@@ -0,0 +1,9 @@
+package com.turkishairlines.technology.dt.route_wings.repository;
+
+import com.turkishairlines.technology.dt.route_wings.model.Transportation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransportationRepository extends JpaRepository<Transportation, Long> {
+}
